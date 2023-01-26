bee65388688b0e86e774b4e188cbb8370bd9ccdf
Peter-Mascherin <pe.mascherin@gmail.com>
2023-01-25 19:31:20 -0500

    base react app, search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SeachBar from "./SearchBar";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+  const updateData = (searchParams) => {
+    setData(searchParams);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SeachBar callback={updateData}></SeachBar>
+      <p>Name: {"name" in data ? data["name"] : "No name to display"}</p>
+      <p>
+        Max Price: {"price" in data ? data["price"] : "No price to display"}
+      </p>
+      <p>Type: {"type" in data ? data["type"] : "No type to display"}</p>
+      <p>Brand: {"brand" in data ? data["brand"] : "No brand to display"}</p>
     </div>
   );
 }
