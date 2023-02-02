85afa1a24f8baaf99c56ef1114870a15aaf2c314
mascherp <mascherp@sheridancollege.ca>
2023-02-02 10:44:14 -0500

    installed bootstrap and styled website

--- src/AddItem.js
+++ src/AddItem.js
@@ -22,41 +22,65 @@ function AddItem(props) {
     setBrand("");
   };
   return (
-    <div>
-      <h2>Add a new Item</h2>
-      <form>
-        <label htmlFor="name-field">Name: </label>
-        <input
-          id="name-field"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label htmlFor="price-field">Price: </label>
-        <input
-          id="price-field"
-          type="number"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
-        <label htmlFor="type-field">Type: </label>
-        <input
-          id="type-field"
-          type="type"
-          value={type}
-          onChange={(e) => setType(e.target.value)}
-        />
-        <label htmlFor="brand-field">Brand: </label>
-        <input
-          id="brand-field"
-          type="text"
-          value={brand}
-          onChange={(e) => setBrand(e.target.value)}
-        />
-        <button type="button" onClick={addItemButtonPressed}>
+    <div className="container">
+      <div className="row ">
+        <h2>Add a new Item</h2>
+      </div>
+
+      <div className="row ">
+        <div className="col">
+          <label htmlFor="name-field">Name: </label>
+          <input
+            id="name-field"
+            type="text"
+            className="form-control"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="price-field">Price: </label>
+          <input
+            id="price-field"
+            type="number"
+            className="form-control"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="type-field">Type: </label>
+          <input
+            id="type-field"
+            type="type"
+            className="form-control"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          />
+        </div>
+        <div className="col">
+          <label htmlFor="brand-field">Brand: </label>
+          <input
+            id="brand-field"
+            type="text"
+            className="form-control"
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="row mt-2">
+        <div className="col-4" />
+
+        <button
+          type="button"
+          className="btn btn-primary col-4"
+          onClick={addItemButtonPressed}
+        >
           Add Item
         </button>
-      </form>
+      </div>
     </div>
   );
 }
